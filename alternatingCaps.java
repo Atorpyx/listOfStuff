598757a75c02bb70430da2e84a6695766ae1a432
Atorpyx <130733987+Atorpyx@users.noreply.github.com>
2023-05-30 22:37:00 -0700

    Update alternatingCaps.java

--- alternatingCaps.java
+++ alternatingCaps.java
@@ -1,6 +1,6 @@
 //ThIs iS WhAt tHe pRoGrAm sPiTs oUt wHeN I InPuT ThIs sEnTeNcE I HoPe yOu fInD ThIs hElPfUl
 public static String alternatingCaps(String word) {
-		for(int i = 0; i < word.length(); i += 2)
-			word = word.substring(0, i) + word.substring(i, i+1).toUpperCase() + word.substring(i + 1);
-		return word;
+	for(int i = 0; i < word.length(); i += 2)
+		word = word.substring(0, i) + word.substring(i, i+1).toUpperCase() + word.substring(i + 1);
+	return word;
 }
