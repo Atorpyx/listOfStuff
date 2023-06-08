f9c9e76af3966a1662ac96c7e81c90bd3f80eb99
Atorpyx <130733987+Atorpyx@users.noreply.github.com>
2023-06-08 15:37:16 -0700

    Update Blackjack.java
    simplified code

--- Blackjack.java
+++ Blackjack.java
@@ -1,3 +1,6 @@
+//Blackjack program with hit, double down, and money system in only 99 lines of code mostly following what's in AP Java subset with exceptions of the Scanner class for input
+//I could have cut lines of code with still maintaining function but I figured it would take away from the readability
+//AP CSA Final project- after 3 weeks of bug finding, brainstorming, and typing, here it is. You'll never win muahahahaha
 import java.util.ArrayList;
 import java.util.Scanner;
 public class BlackJack {
@@ -23,86 +26,75 @@ public class BlackJack {
 		}
 		System.out.println();
 	}
-	private void drawCard(int i) {
-		if(i == 0)
-			dealer.add(cards[(int) (13*Math.random())]);
-		if(i == 1)
-			player.add(cards[(int) (13*Math.random())]);
+	private void drawCard(ArrayList<String> holder) {
+		holder.add(cards[(int) (13*Math.random())]);
 	}
-	private int checkCards(int number) {
+	private int checkCards(ArrayList<String> holder) {
 		int num = 0;
-		boolean hasAce = false;
-		ArrayList<String> holder = dealer;
-		if(number == 0)
-			holder = player;
 		for(String i: holder) {
 			if(i.equals("Q") || i.equals("K") || i.equals("J")) 
 				num += 10;
-			else if(i.equals("A")) {
-			    hasAce = true;
+			else if(i.equals("A"))
 				num++;
-			} else 
+			else 
 				num += Integer.parseInt(i);
 		}
-		if(hasAce && num + 10 <= 21)
+		if(holder.indexOf("A") != -1 && num + 10 <= 21)
 			num += 10;
 		return num;
 	}
 	private void determiner(int bet) {
-		if(checkCards(0) > 21) {
+		if(checkCards(player) > 21) {
 			coins -= bet;
 			System.out.println("You Busted! You lost " + bet + " coins. You now have " + coins + " coins.");
-		} else if(checkCards(1) > 21) {
+		} else if(checkCards(dealer) > 21) {
 			coins += bet;
 			System.out.println("Dealer Busted! You won " + bet + " coins. You now have " + coins + " coins.");
-		} else if(checkCards(0) > checkCards(1)) {
+		} else if(checkCards(player) > checkCards(dealer)) {
 			coins += bet;
 			System.out.println("You won " + bet + " coins. You now have " + coins + " coins.");
-		} else if(checkCards(0) < checkCards(1)) {
+		} else if(checkCards(player) < checkCards(dealer)) {
 			coins -= bet;
 			System.out.println("You lost " + bet + " coins. You now have " + coins + " coins.");
 		} else
 			System.out.println("You drew :/");
 	}
+	private String prompt() {
+		return new Scanner(System.in).nextLine();
+	}
 	public void game(int bet) {
 		dealer.clear();
 		player.clear();
 		boolean stop = true;
-		String lastResult = "";
-		drawCard(0);
-		drawCard(0);
-		drawCard(1);
-		drawCard(1);
+		String option = "";
+		drawCard(dealer);
+		drawCard(dealer);
+		drawCard(player);
+		drawCard(player);
 		viewCards(1);
 		viewCards(0);
 		while(stop) {
 			System.out.println("You wanna hit, stand, or double down?(h/s/d)");
-			String option = new Scanner(System.in).nextLine();
-			if(option.equals("h")) {
-				drawCard(1);
+			option = prompt();
+			if(option.equals("h") || option.equals("d")) {
+				drawCard(player);
 				viewCards(1);
-			} else if(option.equals("d")) {
-				drawCard(1);
-				viewCards(1);
+			}
+			if(!option.equals("h") || checkCards(player) >= 21)
 				stop = false;
-			} else 
-				stop = false;
-			if(checkCards(0) >= 21)
-				stop = false;
-			lastResult = option;
 		}
-		while(checkCards(1) < 17)
-			drawCard(0);
-		if(checkCards(0) > 21)
+		while(checkCards(dealer) < 17)
+			drawCard(dealer);
+		if(checkCards(player) > 21)
 			viewCards(0);
 		else
 			viewCards(2);
-		if(lastResult.equals("d")) 
+		if(option.equals("d")) 
 			determiner(bet*2);
 		else
 			determiner(bet);
 		System.out.println("You want to try again? (y/n)");
-		if(new Scanner(System.in).nextLine().equals("y"))
+		if(prompt().equals("y"))
 			game(bet);
 		else
 			System.out.println("Come back next time!!");
