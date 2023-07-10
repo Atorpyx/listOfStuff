1e0bb18eb50da4ef5da393699bd6ebec42c1dfb1
Atorpyx <130733987+Atorpyx@users.noreply.github.com>
2023-07-10 16:58:17 -0700

    Create isPrime.java

---
+++ isPrime.java
@@ -0,0 +1,18 @@
+//without mod
+public static boolean primeNum(int num) {
+		double numholder = num-1;
+		while((num/numholder)/(int)(num/numholder) != 1 && numholder != 1)
+			numholder--;
+		if(numholder == 1 && num != 1)
+			return true;
+		return false;
+	} 
+//with mod
+	public static boolean primeNum(int num) {
+		double numholder = num-1;
+		while(num % numholder != 0 && numholder > 1)
+			numholder--;
+		if(numholder == 1 && num > 2)
+			return true;
+		return false;
+	}
